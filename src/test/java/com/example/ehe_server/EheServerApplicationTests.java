732db60c12763d7716244c0e162284cd6d135915
RJErik <ritzerikjanos@gmail.com>
2025-01-14 12:34:40 +0100

    Create EheServerApplicationTests.java
    Added EheServerApplicationTests

---
+++ src/test/java/com/example/ehe_server/EheServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ehe_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EheServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
