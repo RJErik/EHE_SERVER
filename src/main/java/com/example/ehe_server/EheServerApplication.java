4027ce9a4d1153fafd70e05b968425f78ce1b2ee
RJErik <ritzerikjanos@gmail.com>
2025-01-14 12:34:07 +0100

    Create EheServerApplication.java
    Added EheServerApplication.

---
+++ src/main/java/com/example/ehe_server/EheServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.ehe_server;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EheServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EheServerApplication.class, args);
+    }
+
+}
