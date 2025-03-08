18192f3e36e11723d15b06ae9d56e953b83658bf
RJErik <ritzerikjanos@gmail.com>
2025-03-08 19:43:54 +0100

    Removed testing remenants.

--- src/main/java/com/example/ehe_server/repository/TestUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ehe_server.repository;
-
-import com.example.ehe_server.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestUserRepository extends JpaRepository<User, Integer> {
-}
